95faca3f6467ccd54bbfb5b5beda41b8a8eae319
Emil155rf <levissctt@yandex.ru>
2024-09-22 19:31:55 +0300

    calculator js

---
+++ calculator/main.js
@@ -0,0 +1,54 @@
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const minusBtn = document.getElementById('minus')
+const plusBtn = document.getElementById('plus')
+const multiplyBtn = document.getElementById('multiply')
+const divideBtn = document.getElementById('divide')
+let action = '+'
+
+
+multiplyBtn.style.backgroundColor = 'green'
+multiplyBtn.style.borderColor = 'green'
+
+plusBtn.onclick = function() {
+    action = '+'
+} 
+minusBtn.onclick = function() {
+    action = '-'
+} 
+multiplyBtn.onclick = function() {
+    action = '*'
+} 
+divideBtn.onclick = function() {
+    action = '/'
+} 
+
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result 
+}
+
+function computeWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    if (actionSymbol == '+') {
+        return num1 + num2
+    } else if (actionSymbol == '-') {
+        return num1 - num2
+    } else if (actionSymbol == '*') {
+        return num1 * num2
+    } else if (actionSymbol == '/') {
+        return num1 / num2
+    }
+}
+
+submitBtn.onclick = function() {
+    const result = computeWithAction(input1, input2, action)
+    printResult(result)
+}
